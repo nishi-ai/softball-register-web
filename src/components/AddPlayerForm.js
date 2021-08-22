6b2701666aa9090d498334da062b8a193970f1bb
Ai Nishikawa <60405643+nishi-ai@users.noreply.github.com>
2021-08-22 15:51:14 +0200

    added validation on frontend as well and render error messages for name and email input

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -1,45 +1,100 @@
 import { React } from 'react';
 // directly access to reference cue DOM elements
-import { useRef } from 'react';
 import { Form, Button, Spinner } from 'react-bootstrap';
 
-function AddPlayerForm(props) {
-    const nameInputRef = useRef();
-    const emailInputRef = useRef();
+import useInput from '../hooks/use-input'
 
-    // disable button in case there's an http request running
-    // use the useEffect hook to set a state defalt: false
-    
+function AddPlayerForm(props) {
+    // use hook useInput
+    const {
+        value: enteredName,
+        isValid: enteredNameIsValid,
+        hasError: nameInputHasError,
+        valueChangeHandler: nameChangeHandler,
+        inputBlurHanlder: nameBlurHanlder,
+        reset: resetNameInput
+    } = useInput(value => value.trim() !== '' && value.length >= 2);
+
+    const {
+        value: enteredEmail,
+        isValid: enteredEmailIsValid,
+        hasError: EmailInputHasError,
+        valueChangeHandler: emailChangeHandler,
+        inputBlurHanlder: emailBlurHanlder,
+        reset: resetEmailInput
+    } = useInput(value => value.includes('@'));
+
+    // to manage overall form validity
+    let formIsValid = false;
+
+    if (enteredNameIsValid && enteredEmailIsValid) {
+        formIsValid = true;
+    }
 
     const submitHandler = (event) => {
-        // prevent the browser default to allow to fully handle the submission
+        // prevent the browser default of senfing a request,  to allow to fully handle the submission
         event.preventDefault();
-        
-        const enteredName = nameInputRef.current.value;
-        const enteredEmail = emailInputRef.current.value;
-     
+
+        if (!formIsValid) {
+            return;
+        }
+
+        // if enteredNameIsValid is valid, just return and stop here
+        // if (props.showNameErrorMessage) {
+        //     console.log('showNameErrorMessage true', props.showNameErrorMessage)
+        //     // return;
+        // } else {
+        //     console.log('showNameErrorMessage false', props.showNameErrorMessage)
+        // }
+             
         // Rest API to send server & DB
         const playerData = {
             name: enteredName,
             email: enteredEmail
         };
-   
-        props.onAddPlayer(playerData) 
-    }
+
+        props.onAddPlayer(playerData);
+
+        // reset states
+        resetNameInput();
+        resetEmailInput();
+    };
 
         return (
-            <Form onSubmit={submitHandler}>
-                
+            <Form noValidate onSubmit={submitHandler} validated={enteredNameIsValid && enteredEmailIsValid}>
                 <Form.Group className="mb-3">
                     <Form.Label htmlFor='playername'>Name</Form.Label>
-                    <Form.Control type="name" placeholder="taro" ref={nameInputRef} />
+                    <Form.Control
+                        type="name"
+                        id="name"
+                        placeholder="taro"
+                        onChange={nameChangeHandler}
+                        onBlur={nameBlurHanlder}
+                        value={enteredName}
+                    />
+                    {(props.showNameErrorMessage || nameInputHasError) && (
+                     <p className='error-text'>
+                         More than 2 letters required
+                    </p>
+                    )}
                 </Form.Group>
+    
                 <Form.Group className="mb-3">
                     <Form.Label htmlFor='email'>Email</Form.Label>
-                    <Form.Control type="email" placeholder="name@example.com" ref={emailInputRef} />
+                    <Form.Control 
+                        type="email"
+                        id="email"
+                        placeholder="name@example.com"
+                        onChange={emailChangeHandler}
+                        onBlur={emailBlurHanlder}
+                        value={enteredEmail}
+                    />
+                    {(props.showEmailErrorMessage || EmailInputHasError) && (
+                     <p className='error-text'>Please enter a valid email with @.</p>
+                    )}
                 </Form.Group>
                 
-                <Button variant="outline-primary" type="submit" disabled={props.callLoading}>
+                <Button variant="outline-primary" type="submit" disabled={!formIsValid || props.callLoading}>
                 {props.callLoading && (
                     <Spinner
                     as="span"
---
+++ src/hooks/use-input.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+// check name and emai are valid or not
+const useInput = (validateValue) => {
+    // whenever one of these states changes, this component function gets re-rendered
+    const [enteredValue, setEnteredValue] = useState('');
+
+    // set input field was touched, so the error message can be already shown before submitting and prevent not show before entering input
+    const [isTouched, setIsTouched] = useState(false);
+
+    const valueIsValid = validateValue(enteredValue);
+
+    // once submitted clicked, all inputs are treated as touched, whatever they are valid or not
+    const hasError = !valueIsValid && isTouched;
+
+    const valueChangeHandler = (e) => {
+        setEnteredValue(e.target.value);
+    }
+
+    // prevent input from losing and show errors at every editting
+    const inputBlurHanlder = (e) => {
+        // input field is touched once
+        setIsTouched(true);
+    }
+
+    // reset the touched state to false once submitted
+    const reset = () => {
+        setEnteredValue('');
+        setIsTouched(false)
+    }
+
+    // can be called from the components that use the hook
+    return {
+        value: enteredValue,
+        isValid: valueIsValid,
+        hasError,
+        valueChangeHandler,
+        inputBlurHanlder,
+        reset
+    };
+};
+
+export default useInput;
