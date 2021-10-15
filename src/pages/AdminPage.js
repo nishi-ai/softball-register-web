6e9838437996bff965f45791b1443325e41b22ab
Ai Nishikawa <60405643+nishi-ai@users.noreply.github.com>
2021-10-15 14:35:17 +0200

    deleted my effort and wasted time

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import Table from '../components/Table'
 
 function AdminPage (props) { 
     
     const players = props.playersList
     console.log('AdminPage', players)
-    // get header of table id, name, email
-    const theadData = Object.keys(players[0]);
-    // make an object include id for each key and info of each player
-    const tbodyData = players.map((player, index) => {
-        return {
-            id: index,
-            items: [index + 1, player.name, player.email]
-        }
-    })
+
     return (
-        <div>
-            <Table theadData={theadData} tbodyData={tbodyData} />
-        </div>
+        <table>
+            {players.map(player => (
+            <tr key={player.id}>  
+                <td>{player.name}</td>
+                <td>{player.email}</td>
+            </tr>
+            ))}
+        </table>
+
     );
 }
 
