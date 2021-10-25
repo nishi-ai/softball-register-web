080af3797a275b9c191c47a8ad38b77f93466a10
Bright Lee <benevbright@gmail.com>
2021-10-25 19:53:55 +0200

    fix mobile layout css

--- src/components/BasicInfo.js
+++ src/components/BasicInfo.js
@@ -25,28 +25,24 @@ function DisplayBasicInfo (props) {
                 <Carousel>
                 <Carousel.Item>
                     <img
-                    className="d-block w-100"
                     src={eachImage[0]}
                     alt="First slide"
                     />
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
-                    className="d-block w-100"
                     src={eachImage[1]}
                     alt="Second slide"
                     />
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
-                    className="d-block w-100"
                     src={eachImage[2]}
                     alt="Third slide"
                     />
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
-                    className="d-block w-100"
                     src={eachImage[3]}
                     alt="Third slide"
                     />
