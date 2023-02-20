a19e7ce171cb8da601fff8ccd9111616871771a1
Ai Nishikawa <aiaimforworld@gmail.com>
2023-02-20 10:37:35 +0100

    remove getEventDataHandler since setServersideProp

--- components/Events/EventInfo.js
+++ components/Events/EventInfo.js
@@ -3,26 +3,10 @@ import EventCard from "./EventCard";
 import styles from "./Event.module.css";
 
 const DisplayEventInfo = (props) => {
-  const [eventData, setEventData] = useState([]);
-
-  const getEventDataHandler = async () => {
-    try {
-      setEventData(props.result);
-    } catch (error) {
-      alert("fetch failed!!!");
-      console.log(error);
-      window.location = "/500";
-    }
-  };
-  // call get data handler in the useEffect hook which runs once when the component is mounted.
-  useEffect(() => {
-    getEventDataHandler();
-  }, []);
-
   return (
     <div>
       <div className={styles.eventContainer}>
-        <EventCard eventData={eventData} />
+        <EventCard eventData={props.result} />
       </div>
     </div>
   );
